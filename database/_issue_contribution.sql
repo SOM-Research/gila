a60d60a1255eb4ddd19c45a4fc6153e6bf812b1d
Belen Rolandi <Belen Rolandi@192.168.38.39>
2014-06-12 14:39:37 +0200

    Changes to script issue_contribution

--- database/_issue_contribution.sql
+++ database/_issue_contribution.sql
@@ -7,10 +7,12 @@ cant_collab int(11)
 ;
 
 insert into _issue_contribution
+
 -- selects the issue id, the number of contributors involved in the issue and the number of contributors that are collaborators
 -- the number of contributors is calculated counting the number of comments the issue has from distinct users
 -- the number of collaborators is calculated obtaining all the (distinct) contributors that have assigned the role collaborator
 -- also includes those issues that have not received any comment
+
 select issue_contrib.issue_id, issue_contrib. issue_num, count(distinct issue_contrib.contributors) as cant_contrib, 
 count(distinct issue_collab.collaborators) as cant_collab
 from
