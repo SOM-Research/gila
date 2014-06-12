07a119176187feece1c5d636721393ae6574516d
Belen Rolandi <Belen Rolandi@192.168.38.39>
2014-06-12 14:40:54 +0200

    Changes to script issue_resolution

--- database/_issue_resolution.sql
+++ database/_issue_resolution.sql
@@ -1,6 +1,7 @@
 create table _issue_resolution(
 issue_id int(11),
-resolution varchar(15)
+resolution varchar(15),
+solved_by int(11)
 ) ENGINE=InnoDB
 ;
 
@@ -10,26 +11,26 @@ insert into _issue_resolution
 -- possible resolution values are: 'merged', 'closed' and 'pending'
 
 -- selects as merged all those issues that have associated a merged event
-select distinct ie.issue_id, 'merged' as resolution
+select distinct ie.issue_id, 'merged' as resolution, ie.actor_id as solved_by
 from issue_events ie
 where ie.action = 'merged'
 
 union
 
--- selects as closed all those issues that have associated a closed action, and this action is the latest action for the issue
+-- selects as closed all those issues that have associated a closed action, and don't have a posterior reopen action
 -- and the issue does not have a merged event attached
-select distinct ie.issue_id, 'closed' as resolution
+select distinct ie.issue_id, 'closed' as resolution, ie.actor_id as solved_by
 from issue_events ie
-where ie.created_at >= all (select ie1.created_at from issue_events ie1 where ie1.issue_id = ie.issue_id) 
+where ie.created_at > ifnull((select max(ie1.created_at) from issue_events ie1 where ie1.issue_id = ie.issue_id and ie1.action = 'reopened'),'0000-00-00 00:00:00') 
 	  and ie.action = 'closed'
 	  and not exists (select ie2.issue_id from issue_events ie2 where ie2.action = 'merged' and ie2.issue_id = ie.issue_id)
 
 union
-
--- selects as pending all those issues that have not been merged and don't have a closed action as its latest action
-select distinct ie.issue_id, 'pending' as resolution
+-- selects as pending all those issues that have not been merged or closed
+-- or that have a reopened action posterior to all closed actions
+select distinct ie.issue_id, 'pending' as resolution, null as solved_by
 from issue_events ie
-where ie.created_at >= all (select ie1.created_at from issue_events ie1 where ie1.issue_id = ie.issue_id) 
-	  and ie.action <> 'closed'
-	  and not exists (select ie2.issue_id from issue_events ie2 where ie2.action = 'merged' and ie2.issue_id = ie.issue_id)
+where not exists (select ie1.issue_id from issue_events ie1 where ie1.issue_id = ie.issue_id and (ie1.action = 'merged' or ie1.action = 'closed'))
+	  or exists (select ie2.issue_id from issue_events ie2 where ie2.issue_id = ie.issue_id and ie2.action = 'reopened' and ie2.created_at > 
+	  ifnull((select max(ie3.created_at) from issue_events ie3 where ie3.issue_id = ie.issue_id and ie3.action = 'closed'),'0000-00-00 00:00:00'))
 ;
