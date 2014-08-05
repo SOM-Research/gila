71fce0dd0b50c8e211b0e57f2320bb07e5434cf6
jlcanovas <me@jlcanovas.es>
2014-08-05 13:19:56 +0200

    First version of the web

--- database/lb_num_created_label_issues_user.sql
+++
@@ -1,9 +0,0 @@
-
--- selects the number of issues a user has created for each label
- 
-create view num_created_label_issues_user as
-select label_id, created_by, count(created_by) as num_issues from label_issues
-where created_by is not null
-group by label_id, created_by
-order by label_id, num_issues desc
-;
