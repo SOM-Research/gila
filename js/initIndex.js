771d8f8262a697be60a7677f82ca95bf887ac071
belenrolandi <belenrolandi@gmail.com>
2014-08-07 13:17:00 +0200

    Cha

--- js/initIndex.js
+++ js/initIndex.js
@@ -1,4 +1,5 @@
-var labelAnalyzerServlet = 'http://atlanmodexp.info.emn.fr:8800/ladrian';
+var labelAnalyzerServlet = 'http://localhost:8080/labelAnalysisServer';
+//var labelAnalyzerServlet = 'http://atlanmodexp.info.emn.fr:8800/gila';
 var selectedProjectName = "";
 
 window.onload = function() {
