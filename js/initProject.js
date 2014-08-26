1a287b97909cc6240ca744a32a66ce813cdb8772
valeriocos <valerio.cosentino@gmail.com>
2014-08-26 20:13:01 +0200

    bug fixing for imagePreviewOnHover.js adding error page

--- js/initProject.js
+++ js/initProject.js
@@ -10,40 +10,47 @@ window.onload = function() {
 	    for (var i = 0; i < parts.length; i++) {
 	        var nv = parts[i].split('=');
 	        if (!nv[0]) continue;
-	        params[nv[0]] = nv[1] || true;
+	        params[nv[0]] = nv[1] || -1;
 	    }
 	}
 	
-	if(params.projectId) {
-		projectId = params.projectId;
-		if(params.projectName) {
-			$("#projectName").text(params.projectName + " results");
-		} else {
-			$("#projectName").text('The results for your project');
-		}
-		
-		loadPage(projectId);
-
-	} else {
-		if(params.projectName) {
-			$("#projectName").text(params.projectName + " results");
-			$.ajax({
-				  url: labelAnalyzerServlet + "/LabelAnalysisServlet?event=getprojectid&project=" + params.projectName,
-				  success:function(data) {
-					projectId = data[0].projectId;
-				    loadPage(projectId);
-				}
-			});
-			
-	} else {
-			$("#projectName").text('No project found');
-		}
+	//if project id not found redirect the user to the home page
+	if (params.projectId === -1) {
+		window.location.replace("/error_page.html");
 	}
+	//else redirect the user to the project page
+	else {
+		if(params.projectId) {
+			projectId = params.projectId;
+			if(params.projectName) {
+				$("#projectName").text(params.projectName + " results");
+			} else {
+				$("#projectName").text('The results for your project');
+			}
+			
+			loadPage(projectId);
+
+		} else {
+			if(params.projectName) {
+				$("#projectName").text(params.projectName + " results");
+				$.ajax({
+					  url: labelAnalyzerServlet + "/LabelAnalysisServlet?event=getprojectid&project=" + params.projectName,
+					  success:function(data) {
+						projectId = data[0].projectId;
+						loadPage(projectId);
+					}
+				});
+				
+		} else {
+				$("#projectName").text('No project found');
+			}
+		}
+		}
 }
 	//var projectId = location.search.split('projectId=')[1];
 
 function loadPage(projectId) {
-
+	
 	generaterq1(projectId);
 	
 	var source =
